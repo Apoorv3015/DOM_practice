39b146cce03942950d055575efa1627015696bef
apoorv3015 <apoorvposwal99@gmail.com>
2022-12-24 13:26:56 +0530

    List item's styling changed using getElementByTagName

--- main.js
+++ main.js
@@ -5,10 +5,18 @@
 // title.style.color = "green";
 // title.style.fontWeight = "bold";
 
-const li = document.querySelectorAll('.list-group-item');
-console.log(li[2]);
+// const li = document.querySelectorAll('.list-group-item');
+// console.log(li[2]);
+// li[2].style.backgroundColor = "green";
+
+// li.forEach((item)=>{
+//     item.style.fontWeight = "bold";
+// });
+
+const li = document.getElementsByTagName('li');
+console.log(li);
 li[2].style.backgroundColor = "green";
 
-li.forEach((item)=>{
-    item.style.fontWeight = "bold";
-});
+for(let i=0;i<li.length;i++){
+    li[i].style.fontWeight = "bold";
+}
