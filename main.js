25d78e321333b76b41fd1e8fd57693c930c19e7b
apoorv3015 <apoorvposwal99@gmail.com>
2022-12-24 13:16:24 +0530

    List items style changed

--- main.js
+++ main.js
@@ -1,6 +1,14 @@
 
-const headtitle = document.getElementById('main-header');
-const title = document.querySelector('.title');
-headtitle.style.borderBottom = "3px solid black";
-title.style.color = "green";
-title.style.fontWeight = "bold";
+// const headtitle = document.getElementById('main-header');
+// const title = document.querySelector('.title');
+// headtitle.style.borderBottom = "3px solid black";
+// title.style.color = "green";
+// title.style.fontWeight = "bold";
+
+const li = document.querySelectorAll('.list-group-item');
+console.log(li[2]);
+li[2].style.backgroundColor = "green";
+
+li.forEach((item)=>{
+    item.style.fontWeight = "bold";
+});
