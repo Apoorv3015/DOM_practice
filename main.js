0c49b75839050b981b558f65fa97e17448248de1
apoorv3015 <apoorvposwal99@gmail.com>
2022-12-24 16:15:00 +0530

    List Item styling changes using querySelector and querySelectorAll

--- main.js
+++ main.js
@@ -13,10 +13,27 @@
 //     item.style.fontWeight = "bold";
 // });
 
-const li = document.getElementsByTagName('li');
-console.log(li);
-li[2].style.backgroundColor = "green";
+// const li = document.getElementsByTagName('li');
+// console.log(li);
+// li[1].style.backgroundColor = "green";
+// li[2].style.display = "none";
 
-for(let i=0;i<li.length;i++){
-    li[i].style.fontWeight = "bold";
-}
+// for(let i=0;i<li.length;i++){
+//     li[i].style.fontWeight = "bold";
+// }
+
+// const liFirst = document.querySelector('li');
+// const liLast = document.querySelector('li:last-child');
+// const liSecond = document.querySelector('li:nth-child(2)');
+// const liThird = document.querySelector('li:nth-child(3)');
+
+// liSecond.style.backgroundColor = "green";
+// liThird.style.display = "none";
+
+const li = document.querySelectorAll('li');
+li[1].style.color = "green";
+const odd = document.querySelectorAll('li:nth-child(odd)');
+// console.log(odd);
+odd.forEach((item)=>{
+    item.style.backgroundColor = "green";
+});
