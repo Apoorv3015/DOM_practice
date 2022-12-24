0a82d43daa53794de72b11be50d18d642ccd4005
apoorv3015 <apoorvposwal99@gmail.com>
2022-12-24 12:31:54 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,6 @@
+
+const headtitle = document.getElementById('main-header');
+const title = document.querySelector('.title');
+headtitle.style.borderBottom = "3px solid black";
+title.style.color = "green";
+title.style.fontWeight = "bold";
